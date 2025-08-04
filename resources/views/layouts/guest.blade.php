de56dd1d3c5e3a32a228f1d57b7cc47917baefbd
Khalll75 <Khalishaathaya04@gmail.com>
2025-08-04 09:33:22 +0700

    Fixing logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -24,11 +24,10 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-            <div class="mb-6">
-                <a href="/">
-                    <x-application-logo class="w-24 h-24 drop-shadow-lg" />
+            <div class="mb-8 text-center">
+                <a href="/" class="flex justify-center">
+                    <x-application-logo class="drop-shadow-lg" />
                 </a>
-                <h1 class="text-white text-2xl font-bold text-center mt-4">Gudang Properti Jambi</h1>
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-xl overflow-hidden sm:rounded-lg border border-gray-200">
